0ebf166b04173e8a6c0266036b121c3b999c082f
Belinda Brown <belindabrownr04@gmail.com>
2019-07-01 20:41:16 -0600

    Add files via upload

---
+++ indigo_workspace/sandbox/test/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/est/indigo_workspace/sandbox/test/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/est/indigo_workspace/sandbox/test/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/est/indigo_workspace/sandbox/test:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
