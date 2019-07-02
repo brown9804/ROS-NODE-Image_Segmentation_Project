f14db1c1b19bddd887b811844e3ba3e9138d55bc
Belinda Brown <belindabrownr04@gmail.com>
2019-07-01 20:45:40 -0600

    Add files via upload

---
+++ indigo_workspace/sandbox/test/build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/est/indigo_workspace/sandbox/test/lib/libgtest.pdb"
+  "/home/est/indigo_workspace/sandbox/test/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
